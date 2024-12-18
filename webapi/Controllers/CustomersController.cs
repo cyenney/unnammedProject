79c16589f9c4690b9fd6e04d816b35a2220e51fb
cody yenney <cody.yenney@outlook.com>
2024-12-17 19:03:54 -0500

    save

---
+++ webapi/Controllers/CustomersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapi.Models.Customer;
+
+namespace webapi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CustomersController : ControllerBase
+{
+
+    [HttpPost("register")]
+    public ActionResult RegisterCustomer()
+    {
+        return Ok();
+    }
+
+    [HttpPost("login")]
+    public ActionResult CustomerLogin(LoginCustomer customer)
+    {
+        return Ok();
+    }
+
+}
---
+++ webapi/Models/Customer/Customer.cs
@@ -0,0 +1,18 @@
+﻿namespace webapi.Models.Customer
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+
+    public class LoginCustomer
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
